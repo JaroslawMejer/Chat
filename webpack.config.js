ac6835de6e271a769caf6544581989d308d09492
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-07-23 19:30:13 +0200

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+//webpack.config.js
+module.exports = (env) => {
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
+    }
+    return{
+        entry: (env !== 'production' ? [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
+        output: {
+            filename: './bundle.js',
+            path: path.resolve(__dirname, 'public'),
+        },
+        module: {
+        	rules: [
+        		{
+        			test: /\.js$/,
+        			loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+        		},
+                {
+                    test: /\.css$/,
+                    use: [
+                        {loader: 'style-loader'},
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+        	]
+        },
+    }
+};
